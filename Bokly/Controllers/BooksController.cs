ce83ff7752bbdd3162b652be6f060696c9836626
Ling Jiang <kyooryoo@hotmail.com>
2018-10-09 13:00:37 -0400

    data acccess added

--- Bokly/Controllers/BooksController.cs
+++ Bokly/Controllers/BooksController.cs
@@ -2,24 +2,39 @@
 using Bokly.ViewModels;
 using System.Collections.Generic;
 using System.Web.Mvc;
+using System.Data.Entity;
+using System.Linq;
 
 namespace Bokly.Controllers
 {
 	public class BooksController : Controller
 	{
+		public ApplicationDbContext _context;
+
+		public BooksController()
+		{
+			_context = new ApplicationDbContext();
+		}
+
+		protected override void Dispose(bool disposing)
+		{
+			_context.Dispose();
+		}
+
 		public ViewResult Index()
 		{
-			var books = GetBooks();
+			var books = _context.Books.Include(b => b.Genre).ToList();
 			return View(books);
 		}
 
-		private IEnumerable<Book> GetBooks()
+		public ActionResult Details(int id)
 		{
-			return new List<Book>
+			var book = _context.Books.Include(b => b.Genre).SingleOrDefault(b => b.Id == id);
+			if (book == null)
 			{
-				new Book { Id = 1, Name = "Shrek"},
-				new Book { Id = 2, Name = "Wall-e"}
-			};
+				return HttpNotFound();
+			}
+			return View(book);
 		}
 	}
 }
--- Bokly/Models/Genre.cs
+++ Bokly/Models/Genre.cs
@@ -6,15 +6,12 @@ using System.Web;
 
 namespace Bokly.Models
 {
-	public class Customer
+	public class Genre
 	{
-		public int Id { get; set; }
+		public byte Id { get; set; }
+
 		[Required]
 		[StringLength(255)]
 		public string Name { get; set; }
-		public bool IsSubscribed { get; set; }
-		public MembershipType MembershipType { get; set; }
-		public byte MembershipTypeId { get; set; }
-		public DateTime? BirthDate { get; set; }
 	}
 }
